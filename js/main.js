6bc0952ca52aa646e826939c12992bd108e2b0fb
Graham Brooks <grahambrooks@hotmail.co.uk>
2021-07-13 16:58:21 +0100

    --- Auto-Git Commit ---

--- js/main.js
+++ js/main.js
@@ -11,15 +11,28 @@ let paddleX = (canvas.width - paddleWidth) / 2;
 let dx = 2;
 let dy = -2;
 
-document.addEventListener("keydown", keyDownHandler, )
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 
-function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth.paddleHeight);
-    ctx.fillStyle = "#0096dd"
-    ctx.fill();
-    ctc.closePath();
+function keyDownHandler(e) {
+    if (e.key == "right" || e.key == " ArrowRight ") {
+        rightPressed = true;
+
+    } else if (e.key == "left" || e.key == ArrowLeft) {
+        leftPressed = true;
+    }
 }
+
+function keyUpHandler(e) {
+    if (e.key == "right" || e.key == " ArrowRight ") {
+        rightPressed = true;
+
+    } else if (e.key == "left" || e.key == ArrowLeft) {
+        leftPressed = true;
+    }
+
+}
+
 
 function drawBall() {
     ctx.beginPath();
@@ -29,14 +42,35 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth.paddleHeight);
+    ctx.fillStyle = "#0096dd"
+    ctx.fill();
+    ctc.closePath();
+}
+
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPaddle();
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
     if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
         dy = -dy;
+    }
+    if (rightPressed) {
+        paddleX += 7;
+        if (paddleX + paddleWidth > canvas.width) {
+            paddleX = canvas.width - paddleWidth;
+        }
+    } else if (leftPressed) {
+        paddleX -= 7;
+        if (paddleX < 0) {
+            paddleX = 0;
+        }
     }
     x += dx;
     y += dy;
