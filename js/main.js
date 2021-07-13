cf3975e9ecb8b701c7f057a42bcc30e3ab94ce40
Graham Brooks <grahambrooks@hotmail.co.uk>
2021-07-13 16:28:03 +0100

    --- Auto-Git Commit ---

--- js/main.js
+++ js/main.js
@@ -5,9 +5,13 @@ let y = canvas.height - 30;
 let ballRadius = 10;
 let paddleHeight = 10;
 let paddleWidth = 75;
+let rightPressed = false;
+let leftPressed = false;
 let paddleX = (canvas.width - paddleWidth) / 2;
 let dx = 2;
 let dy = -2;
+
+document.addEventListener("keydown", keyDownHandler, )
 
 function drawPaddle() {
     ctx.beginPath();
@@ -17,7 +21,7 @@ function drawPaddle() {
     ctc.closePath();
 }
 
-function drabBall() {
+function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
     ctx.fillStyle = "#009500";
@@ -27,7 +31,7 @@ function drabBall() {
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drabBall();
+    drawBall();
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
