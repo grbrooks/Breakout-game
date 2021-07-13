83886025fb1a4cd61008e22dd3062c054f87047c
Graham Brooks <grahambrooks@hotmail.co.uk>
2021-07-13 15:14:29 +0100

    --- Auto-Git Commit ---

--- js/main.js
+++ js/main.js
@@ -2,9 +2,20 @@ let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 let x = canvas.width / 2;
 let y = canvas.height - 30;
+let ballRadius = 10;
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
 let dx = 2;
 let dy = -2;
-let ballRadius = 10;
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth.paddleHeight);
+    ctx.fillStyle = "#0096dd"
+    ctx.fill();
+    ctc.closePath();
+}
 
 function drabBall() {
     ctx.beginPath();
@@ -17,6 +28,12 @@ function drabBall() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drabBall();
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
     x += dx;
     y += dy;
 }
